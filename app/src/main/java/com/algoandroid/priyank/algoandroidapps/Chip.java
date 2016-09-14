d82036ba934b0e41bcb4d10a586f31e898838618
Priyank Paliwal <paliwal.priyank2001@gmail.com>
2016-09-13 22:04:44 -0400

    chip class

---
+++ app/src/main/java/com/algoandroid/priyank/algoandroidapps/Chip.java
@@ -0,0 +1,19 @@
+package com.algoandroid.priyank.algoandroidapps;
+
+/**
+ * A class that will work as blueprint for chip
+ */
+public class Chip {
+
+    private String first;
+    private String second;
+
+    public Chip(String first, String second) {
+        this.first = first;
+        this.second = second;
+    }
+    public String getSecond() {
+        return second;
+    }
+
+}
